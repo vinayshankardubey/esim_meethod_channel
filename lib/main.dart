4467f2ffed96172374a62fce3b3d4b9eca6c3a16
Hardik Jain <jainakin@Hardiks-MacBook-Air.local>
2024-06-29 01:24:47 +0530

    new method

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,95 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
+import 'dart:io';
 
-import 'package:flutter/services.dart';
-import 'package:flutter_esim/flutter_esim.dart';
+import 'package:flutter/material.dart';
+import 'package:esim_installer_flutter/esim_installer_flutter.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: EsimInstallerHome(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  bool _isSupportESim = false;
-  final _flutterEsimPlugin = FlutterEsim();
+class EsimInstallerHome extends StatefulWidget {
+  @override
+  _EsimInstallerHomeState createState() => _EsimInstallerHomeState();
+}
+
+class _EsimInstallerHomeState extends State<EsimInstallerHome> {
+  String _platformVersion = 'Unknown';
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-    _flutterEsimPlugin.onEvent.listen((event) {
-      print(event);
-    });
+    _initPlatformState();
   }
 
-  Future<void> initPlatformState() async {
-    bool isSupportESim;
+  Future<void> _initPlatformState() async {
+    String platformVersion;
+
     try {
-      isSupportESim = await _flutterEsimPlugin.isSupportESim([]);
-    } on PlatformException {
-      isSupportESim = false;
+      platformVersion = await EsimInstallerFlutter().getPlatformVersion() ??
+          'Unknown platform version';
+    } catch (e) {
+      platformVersion = 'Failed to get platform version.';
     }
 
     if (!mounted) return;
 
     setState(() {
-      _isSupportESim = isSupportESim;
+      _platformVersion = platformVersion;
     });
   }
 
-  Future<void> installEsim() async {
-    await _flutterEsimPlugin.installEsimProfile("LPA:1\$lpa.airalo.com\$TEST");
+  Future<void> _installEsimProfile() async {
+    String result;
+    try {
+      result = await EsimInstallerFlutter().installESimProfile(
+        smdpAddress: 'RSP-0026.OBERTHUR.NET',
+        activationToken: '777FG-KSFV1-F2O3E-CW2X4',
+      );
+    } catch (e) {
+      result = 'Failed to install eSIM profile.';
+    }
+
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Text('isSupportESim: $_isSupportESim\n'),
-              ElevatedButton(
-                onPressed: () {
-                  installEsim();
-                },
-                child: const Text('Install eSim'),
-              )
-            ],
-          ),
+    const String esimUrl =
+        "https://esimsetup.apple.com/esim_qrcode_provisioning?carddata=LPA:1\$RSP-0026.OBERTHUR.NET\$777FG-KSFV1-F2O3E-CW2X4";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('eSIM Installer'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Running on: $_platformVersion\n'),
+            ElevatedButton(
+              onPressed: () {
+                if (Platform.isIOS) {
+                  launchUrlString(
+                    esimUrl,
+                    mode: LaunchMode.externalApplication,
+                  );
+                } else {
+                  _installEsimProfile;
+                }
+              },
+              child: const Text('Install eSIM Profile'),
+            ),
+          ],
         ),
       ),
     );
