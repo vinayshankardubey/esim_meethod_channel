aa28309e423095ce703f35c445b8b6710332bc13
vinay <vinaydevs00@gmail.com>
2024-06-29 12:57:22 +0530

    u

--- android/app/src/main/kotlin/com/example/esim/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/esim/MainActivity.kt
@@ -1,5 +1,39 @@
 package com.example.esim
 
+import android.os.Bundle
+import android.telephony.euicc.EuiccManager
+import android.telephony.euicc.DownloadableSubscription
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "com.example.esim/esim"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "installEsim") {
+                val activationCode = call.argument<String>("activationCode")
+                if (activationCode != null) {
+                    installEsim(activationCode)
+                    result.success("Success")
+                } else {
+                    result.error("INVALID_CODE", "Activation code is null", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun installEsim(activationCode: String) {
+        val euiccManager = getSystemService(EuiccManager::class.java)
+        val downloadableSubscription = DownloadableSubscription.forActivationCode(activationCode)
+        val intentSender = null // This can be used to handle success/failure callbacks if needed
+
+        euiccManager.downloadSubscription(downloadableSubscription, true, intentSender)
+    }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,39 @@
-import 'dart:io';
-
-import 'package:esim/esim_installer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_esim/flutter_esim.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  static const platform = MethodChannel('com.example.esim/esim');
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: EsimInstallerHome(),
-    );
-  }
-}
-
-class EsimInstallerHome extends StatefulWidget {
-  const EsimInstallerHome({super.key});
-
-  @override
-  EsimInstallerHomeState createState() => EsimInstallerHomeState();
-}
-
-class EsimInstallerHomeState extends State<EsimInstallerHome> {
-  bool isSupportESim = false;
-  final _flutterEsimPlugin = FlutterEsim();
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-    _flutterEsimPlugin.onEvent.listen((event) {
-      print(event);
-    });
-  }
-
-  Future<void> initPlatformState() async {
-    bool isSupportESim;
-    try {
-      isSupportESim = await _flutterEsimPlugin.isSupportESim([]);
-    } on PlatformException {
-      isSupportESim = false;
-    }
-
-    if (!mounted) return;
-
-    setState(() {
-      isSupportESim = isSupportESim;
-    });
-  }
-
-  Future<void> installEsim() async {
-    await _flutterEsimPlugin.installEsimProfile(
-      "LPA:1\$RSP-0026.OBERTHUR.NET\$777FG-KSFV1-F2O3E-CW2X4",
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    const String esimUrl =
-        "https://esimsetup.apple.com/esim_qrcode_provisioning?carddata=LPA:1\$RSP-0026.OBERTHUR.NET\$777FG-KSFV1-F2O3E-CW2X4";
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('eSIM Installer'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                // if (Platform.isIOS) {
-                //   print('launched');
-                //   launchUrlString(
-                //     esimUrl,
-                //     mode: LaunchMode.externalApplication,
-                //   );
-                // } else {
-                installEsim();
-                // }
-              },
-              child: const Text('Install eSIM Profile'),
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('eSIM Installer'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              installEsim();
+            },
+            child: Text('Install eSIM'),
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> installEsim() async {
+    try {
+      final String activationCode = 'LPA:1\$RSP-0026.OBERTHUR.NET\$777FG-KSFV1-F2O3E-CW2X4';
+      final String result = await platform.invokeMethod('installEsim', {'activationCode': activationCode});
+      print(result); // Print success message
+    } on PlatformException catch (e) {
+      print("Failed to install eSIM: '${e.message}'.");
+    }
   }
 }
